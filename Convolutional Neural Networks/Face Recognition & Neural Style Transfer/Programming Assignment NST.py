8d0b14b4c5f5f6cb82eaf18c7b98afed800711bf
Dixie234 <52049242+Dixie234@users.noreply.github.com>
2024-09-01 19:20:40 +0100

    addition of week 4 program assignment

---
+++ Convolutional Neural Networks/Face Recognition & Neural Style Transfer/Programming Assignment NST.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import scipy.io
+import scipy.misc
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import pprint
+from public_tests import *
+
+def compute_content_cost(content_output, generated_output):
+    """
+    Computes the content cost
+    
+    Arguments:
+    a_C -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image C 
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image G
+    
+    Returns: 
+    J_content -- scalar that you compute using equation 1 above.
+    """
+    a_C = content_output[-1]
+    a_G = generated_output[-1]
+    
+    ### START CODE HERE
+    
+    # Retrieve dimensions from a_G (≈1 line)
+    _, n_H, n_W, n_C = a_G.get_shape().as_list()
+    
+    # Reshape 'a_C' and 'a_G' (≈2 lines)
+    # DO NOT reshape 'content_output' or 'generated_output'
+    a_C_unrolled = tf.reshape(a_C, shape=[1, n_H * n_W, n_C])
+    a_G_unrolled = tf.reshape(a_G, shape=[1, n_H * n_W, n_C])
+    
+    # compute the cost with tensorflow (≈1 line)
+    J_content = (1 / (4 * n_H * n_W * n_C)) * (tf.reduce_sum(tf.square(tf.subtract(a_C_unrolled, a_G_unrolled))))
+    
+    ### END CODE HERE
+    
+    return J_content
+
+def gram_matrix(A):
+    """
+    Argument:
+    A -- matrix of shape (n_C, n_H*n_W)
+    
+    Returns:
+    GA -- Gram matrix of A, of shape (n_C, n_C)
+    """  
+    ### START CODE HERE
+    
+    #(≈1 line)
+    GA = tf.matmul(A, A, transpose_b=True)
+    
+    ### END CODE HERE
+
+    return GA
